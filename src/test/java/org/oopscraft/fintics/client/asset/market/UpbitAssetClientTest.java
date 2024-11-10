43ff8d0e0ac4c6be6aac7f4af54ec777cecf5f79
chomookun <chomookun@gmail.com>
2024-11-10 18:44:53 +0900

    adds upbit asset client test case

--- src/test/java/org/oopscraft/fintics/client/asset/market/UpbitAssetClientTest.java
+++ src/test/java/org/oopscraft/fintics/client/asset/market/UpbitAssetClientTest.java
@@ -1,7 +1,45 @@
 package org.oopscraft.fintics.client.asset.market;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.oopscraft.arch4j.core.common.test.CoreTestSupport;
+import org.oopscraft.fintics.FinticsConfiguration;
+import org.oopscraft.fintics.client.asset.AssetClientProperties;
+import org.oopscraft.fintics.model.Asset;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UpbitAssetClientTest {
+@SpringBootTest(classes = FinticsConfiguration.class)
+@RequiredArgsConstructor
+@Slf4j
+class UpbitAssetClientTest extends CoreTestSupport {
+
+    private final AssetClientProperties assetClientProperties;
+
+    public UpbitAssetClient getUpbitAssetClient() {
+        return new UpbitAssetClient(assetClientProperties);
+    }
+
+    @Disabled
+    @Test
+    void getAssets() {
+        // given
+        // when
+        List<Asset> assets = getUpbitAssetClient().getAssets();
+        // then
+        log.info("assets: {}", assets);
+        assertTrue(assets.size() > 0);
+        assertTrue(assets.stream().allMatch(asset ->
+                asset.getAssetId() != null &&
+                        asset.getName() != null &&
+                        asset.getMarket() != null &&
+                        asset.getExchange() != null &&
+                        asset.getType() != null));
+    }
 
 }
