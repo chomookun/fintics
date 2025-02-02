48fd21a4f4bc5cbbf2f190babe0b3eafa83723fe
Chomookun <chomookun@gmail.com>
2025-02-02 16:54:11 +0900

    adds dividends feature (#8)

--- src/main/java/org/chomookun/fintics/model/Dividend.java
+++ src/main/java/org/chomookun/fintics/model/Dividend.java
@@ -2,6 +2,7 @@ package org.chomookun.fintics.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.chomookun.fintics.dao.DividendEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -15,5 +16,18 @@ public class Dividend {
     private LocalDate date;
 
     private BigDecimal dividendPerShare;
+
+    /**
+     * factory method
+     * @param dividendEntity dividend entity
+     * @return dividend
+     */
+    public static Dividend from(DividendEntity dividendEntity) {
+        return Dividend.builder()
+                .assetId(dividendEntity.getAssetId())
+                .date(dividendEntity.getDate())
+                .dividendPerShare(dividendEntity.getDividendPerShare())
+                .build();
+    }
 
 }
