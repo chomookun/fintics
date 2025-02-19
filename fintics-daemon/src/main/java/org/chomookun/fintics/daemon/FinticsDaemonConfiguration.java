82fe514ac13a17fb10fa7f8680bf581426855a57
chomookun <chomookun@gmail.com>
2025-02-19 17:56:10 +0900

    fixes daemon test case error

--- fintics-daemon/src/main/java/org/chomookun/fintics/daemon/FinticsDaemonConfiguration.java
+++ fintics-daemon/src/main/java/org/chomookun/fintics/daemon/FinticsDaemonConfiguration.java
@@ -4,11 +4,16 @@ import org.chomookun.arch4j.daemon.DaemonConfiguration;
 import org.chomookun.fintics.core.FinticsCoreConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({FinticsCoreConfiguration.class, DaemonConfiguration.class})
+@ComponentScan(
+        nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
 @EnableConfigurationProperties(FinticsDaemonProperties.class)
 @EnableAutoConfiguration
 public class FinticsDaemonConfiguration {
--- fintics-web/src/main/java/org/chomookun/fintics/web/FinticsWebConfiguration.java
+++ fintics-web/src/main/java/org/chomookun/fintics/web/FinticsWebConfiguration.java
@@ -2,11 +2,18 @@ package org.chomookun.fintics.web;
 
 import org.chomookun.arch4j.web.WebConfiguration;
 import org.chomookun.fintics.core.FinticsCoreConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({FinticsCoreConfiguration.class, WebConfiguration.class})
+@ComponentScan(
+        nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+@EnableAutoConfiguration
 public class FinticsWebConfiguration {
 
 }
