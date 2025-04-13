917ec8e64505daa79a291ea0b68bce1040c2215d
chomookun <chomookun@gmail.com>
2025-04-14 00:26:22 +0900

    cicd:test deploy

--- fintics-core/src/main/java/org/chomookun/fintics/core/FinticsCoreProperties.java
+++ fintics-core/src/main/java/org/chomookun/fintics/core/FinticsCoreProperties.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Fintics core properties
  */
 @ConfigurationProperties(prefix = "fintics.core")
+@Lazy(false)
 @AllArgsConstructor
 @Getter
 public final class FinticsCoreProperties {
