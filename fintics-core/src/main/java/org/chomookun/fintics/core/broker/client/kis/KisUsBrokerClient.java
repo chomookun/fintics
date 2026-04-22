61e06328a1ab7b194285abdc4f66dd106be0cca0
chomookun <chomookun@gmail.com>
2026-04-23 02:24:24 +0900

    feat: adds rate limit borker properties

--- fintics-core/src/main/java/org/chomookun/fintics/core/broker/client/kis/KisUsBrokerClient.java
+++ fintics-core/src/main/java/org/chomookun/fintics/core/broker/client/kis/KisUsBrokerClient.java
@@ -108,7 +108,6 @@ public class KisUsBrokerClient extends BrokerClient {
      */
     private void sleep() throws InterruptedException {
         long sleepMillis = 1000 / this.rateLimit;
-//        long sleepMillis = production ? 200 : 2_000;    // 메뉴얼상은 초당 20건 제약으로 하나 10건만 보내도 응답거부가 많음으로 초당 5건으로 처리
         KisAccessThrottler.sleep(appKey, sleepMillis);
     }
 
